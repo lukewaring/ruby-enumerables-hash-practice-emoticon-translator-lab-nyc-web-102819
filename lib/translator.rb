70805aae0f4af37e528b460ef816dd49fee0279b
Luke Waring <lukewaring@gmail.com>
2019-10-18 21:48:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,64 +1,64 @@
 require 'pry'
 require 'yaml'
 
-def load_library(file_path)
-  emoticons_file = YAML.load_file(file_path)
-  
-  japanese_emoticons = emoticons_file.map do |meaning, emoticons|
-    emoticons[1]
-  end
-  
-  english_emoticons = emoticons_file.map do |meaning, emoticons|
-    emoticons[0]
-  end
-  
-  meanings = emoticons_file.map do |meaning, emoticons|
-    meaning
-  end
-  
-  hash = {
-    :get_meaning => {
-      japanese_emoticons[0] => meanings[0],
-      japanese_emoticons[1] => meanings[1],
-      japanese_emoticons[2] => meanings[2],
-      japanese_emoticons[3] => meanings[3],
-      japanese_emoticons[4] => meanings[4],
-      japanese_emoticons[5] => meanings[5],
-      japanese_emoticons[6] => meanings[6],
-      japanese_emoticons[7] => meanings[7],
-      japanese_emoticons[8] => meanings[8],
-      japanese_emoticons[9] => meanings[9],
-      japanese_emoticons[10] => meanings[10],
-      japanese_emoticons[11] => meanings[11],
-      japanese_emoticons[12] => meanings[12]
-    },
-    :get_emoticon => {
-      english_emoticons[0] => japanese_emoticons[0],
-      english_emoticons[1] => japanese_emoticons[1],
-      english_emoticons[2] => japanese_emoticons[2],
-      english_emoticons[3] => japanese_emoticons[3],
-      english_emoticons[4] => japanese_emoticons[4],
-      english_emoticons[5] => japanese_emoticons[5],
-      english_emoticons[6] => japanese_emoticons[6],
-      english_emoticons[7] => japanese_emoticons[7],
-      english_emoticons[8] => japanese_emoticons[8],
-      english_emoticons[9] => japanese_emoticons[9],
-      english_emoticons[10] => japanese_emoticons[10],
-      english_emoticons[11] => japanese_emoticons[11],
-      english_emoticons[12] => japanese_emoticons[12]
-    }
-  }
-end
-
 # def load_library(file_path)
 #   emoticons_file = YAML.load_file(file_path)
-#   hash = {"get_meaning" => {}, "get_emoticon" => {}}
-#   emoticons_file.each do |meaning, emoticons|
-#     hash["get_meaning"][emoticons[1]] = meaning
-#     hash["get_emoticon"][emoticons[0]] = emoticons[1]
+  
+#   japanese_emoticons = emoticons_file.map do |meaning, emoticons|
+#     emoticons[1]
 #   end
-#   hash
+  
+#   english_emoticons = emoticons_file.map do |meaning, emoticons|
+#     emoticons[0]
+#   end
+  
+#   meanings = emoticons_file.map do |meaning, emoticons|
+#     meaning
+#   end
+  
+#   hash = {
+#     :get_meaning => {
+#       japanese_emoticons[0] => meanings[0],
+#       japanese_emoticons[1] => meanings[1],
+#       japanese_emoticons[2] => meanings[2],
+#       japanese_emoticons[3] => meanings[3],
+#       japanese_emoticons[4] => meanings[4],
+#       japanese_emoticons[5] => meanings[5],
+#       japanese_emoticons[6] => meanings[6],
+#       japanese_emoticons[7] => meanings[7],
+#       japanese_emoticons[8] => meanings[8],
+#       japanese_emoticons[9] => meanings[9],
+#       japanese_emoticons[10] => meanings[10],
+#       japanese_emoticons[11] => meanings[11],
+#       japanese_emoticons[12] => meanings[12]
+#     },
+#     :get_emoticon => {
+#       english_emoticons[0] => japanese_emoticons[0],
+#       english_emoticons[1] => japanese_emoticons[1],
+#       english_emoticons[2] => japanese_emoticons[2],
+#       english_emoticons[3] => japanese_emoticons[3],
+#       english_emoticons[4] => japanese_emoticons[4],
+#       english_emoticons[5] => japanese_emoticons[5],
+#       english_emoticons[6] => japanese_emoticons[6],
+#       english_emoticons[7] => japanese_emoticons[7],
+#       english_emoticons[8] => japanese_emoticons[8],
+#       english_emoticons[9] => japanese_emoticons[9],
+#       english_emoticons[10] => japanese_emoticons[10],
+#       english_emoticons[11] => japanese_emoticons[11],
+#       english_emoticons[12] => japanese_emoticons[12]
+#     }
+#   }
 # end
+
+def load_library(file_path)
+  emoticons_file = YAML.load_file(file_path)
+  hash = {"get_meaning" => {}, "get_emoticon" => {}}
+  emoticons_file.each do |meaning, emoticons|
+    hash["get_meaning"][emoticons[1]] = meaning
+    hash["get_emoticon"][emoticons[0]] = emoticons[1]
+  end
+  hash
+end
 
 def get_japanese_emoticon(file_path, emoticon)
     emoticon_hash = load_library(file_path)
