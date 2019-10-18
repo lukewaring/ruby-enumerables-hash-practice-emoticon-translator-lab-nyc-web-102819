4d22b609f390f058e4d6adc5b273b7e29b06fd1c
Luke Waring <lukewaring@gmail.com>
2019-10-18 21:48:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -51,13 +51,13 @@ def load_library(file_path)
 end
 
 # def load_library(file_path)
-#   library = YAML.load_file(file_path)
-#   result = {"get_meaning" => {}, "get_emoticon" => {}}
-#   library.each do |meaning, emoticons|
-#     result["get_meaning"][emoticons[1]] = meaning
-#     result["get_emoticon"][emoticons[0]] = emoticons[1]
+#   emoticons_file = YAML.load_file(file_path)
+#   hash = {"get_meaning" => {}, "get_emoticon" => {}}
+#   emoticons_file.each do |meaning, emoticons|
+#     hash["get_meaning"][emoticons[1]] = meaning
+#     hash["get_emoticon"][emoticons[0]] = emoticons[1]
 #   end
-#   result
+#   hash
 # end
 
 def get_japanese_emoticon(file_path, emoticon)
